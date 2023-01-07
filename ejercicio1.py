f29487ea77856156052d944015f26a2adb8f07b4
Walter <jalafwalter1@gmail.com>
2023-01-07 11:25:40 -0300

    agregué el .py

---
+++ ejercicio1.py
@@ -0,0 +1,40 @@
+
+"""
+Problema: Retornar la primer letra que se repite en una carta
+Lógica:
+
+# 1
+Crear un diccionario vacío donde luego vamos a almacenar la clave: letra
+valor: cantidad de veces que se repite esa letra.
+
+Crear un bucle para para recorrer las letras de la carta
+
+condicional  si una letra no está en el contenedor la agrego 
+al contenedor.
+al momento de que sí se encuentre en el contenedor
+la imprimo por pantalla y rompo el ciclo.
+"""
+
+# carta = 'Holaa todo bien'
+# contenedor = []
+# for i in carta:
+#     if i not in contenedor:
+#         contenedor.append(i)
+#     else:
+#         print(i)
+#         break
+
+
+
+"""
+# Problema: Retornar la primer letra que no se repite en una carta
+# Lógica:
+
+
+
+"""
+        
+    
+
+        
+
